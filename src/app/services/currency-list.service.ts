8c070f4ef2b7b424d5e06190b0e89ecb7ec14bb6
Miguel Firmino <miguel.firmino@yahoo.com>
2023-10-09 22:32:40 -0300

    Fixed Service

--- src/app/services/currency-list.service.ts
+++ src/app/services/currency-list.service.ts
@@ -12,22 +12,22 @@ export class CurrencyListService {
   constructor(private http: HttpClient) { }
 
   getAllCurrencies(): Observable<Results> {
-    // let url = "https://miguelartioli.pythonanywhere.com/get-currencies";
-    let url = "http://localhost:5000/get-currencies";
+    // let url = "http://localhost:5000/get-currencies";
+    let url = "https://miguelartioli.pythonanywhere.com/get-currencies";
 
     return this.http.get<Results>(url);
   }
 
   increaseVote(currencyName: string): Observable<Results> {
-    // let url = "https://miguelartioli.pythonanywhere.com/vote/" + currencyName;
-    let url = "http://localhost:5000/vote/" + currencyName;
+    // let url = "http://localhost:5000/vote/" + currencyName;
+    let url = "https://miguelartioli.pythonanywhere.com/vote/" + currencyName;
 
     return this.http.put<Results>(url, {});
   }
 
   decreaseVote(currencyName: string): Observable<Results> {
-    // let url = "https://miguelartioli.pythonanywhere.com/remove-vote/" + currencyName;
-    let url = "http://localhost:5000/remove-vote/" + currencyName;
+        // let url = "http://localhost:5000/remove-vote/" + currencyName;
+    let url = "https://miguelartioli.pythonanywhere.com/remove-vote/" + currencyName;
 
     return this.http.put<Results>(url, {});
   }
